b6a52b4f17066295bcf0c5b541236c8b73a636d6
sanaz <sanazzarei1995@gmail.com>
2024-01-13 14:44:40 +0100

    add main page

---
+++ project/src/pages/MainPAge.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import CodeBox from "../components/CodeBox";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
+import "./MainPage.css";
+
+const MainPage = () => {
+  const [code, setCode] = useState("");
+
+  return (
+    <div className="container">
+      <div className="btn-container">
+        <Button label="Save" />
+        <Button label="Share" />
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <p>Home</p>
+        </Link>
+      </div>
+      <CodeBox code={code} onUpdateCode={setCode} />
+    </div>
+  );
+};
+
+export default MainPage;
